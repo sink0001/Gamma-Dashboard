685c22472626aa07ebe7fdc7d03ce406e2f86e17
sink <sink_dev@proton.me>
2025-12-13 20:39:27 +0000

    design base.html

--- backend/routes/views.py
+++ backend/routes/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint("views", __name__)
 
 
 @views.route("/")
 def home_page():
-    return "hello"
+    return render_template("base.html")
