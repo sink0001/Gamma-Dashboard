bfd3ec1f2959822df72e4df4aacf71a6177966a1
sink <sink_dev@proton.me>
2026-01-12 18:52:55 +0000

    make the ticker be in the URL or the analysis page

--- backend/routes/views.py
+++ backend/routes/views.py
@@ -16,8 +16,6 @@ async def cache_searched_stocks_data(ticker: str, session_id: str, heartbeat_int
     finances = await stock.get_finances()
     serialized_data = stock.serialize_finances_for_caching(finances)
     cache_handler.cache(session_id, serialized_data, heartbeat_interval)
-    print(cache_handler.get_key_value(session_id))
-    print(finances)
 
 
 @views.route("/", methods=["GET", "POST"])
@@ -26,23 +24,24 @@ async def home_page():
         return render_template("base.html")
     else:
         # do the logic for determining whether the stock exists or not and if it does store its ticker in session and display the stocks info
-        ticker = request.form.get("searched")
+        ticker = request.form.get("searched").strip() # type: ignore
         if not ticker:
             flash("Please Enter a ticker")
             return render_template("base.html")
         try:
             stock = Stock(ticker, False)
             await cache_searched_stocks_data(ticker, session["session_id"], 60)
-            return redirect(url_for("views.analysis_page"))
+            return redirect(url_for("views.analysis_page", stock_name=ticker))
         except Exception as e:
             flash(e.args[0])
             return render_template("base.html")
 
 
-@views.route("/stock-analysis", methods=["GET", "POST"])
-async def analysis_page():
+@views.route("/stock-analysis/<stock_name>", methods=["GET", "POST"])
+async def analysis_page(stock_name):
     if request.method == "GET": # this can only happen if the ticker exists
-        return render_template("stock-analysis.html") # give all the values
+        print(stock_name)
+        return render_template("stock-analysis.html")
     else:
         '''
         if the ticker exists get and cache the financial statements and stuff
@@ -54,7 +53,7 @@ async def analysis_page():
             return redirect(url_for("views.home_page"))
         try:
             await cache_searched_stocks_data(ticker, session["session_id"], 60)
-            return redirect(url_for("views.analysis_page"))
+            return redirect(url_for("views.analysis_page", stock_name=ticker))
         except Exception as e:
             flash(e.args[0])
             return redirect(url_for("views.home_page"))
