7e040dbd258e72e1ad767581b79c4e5a16296144
sink <sink_dev@proton.me>
2026-01-31 23:29:42 +0000

    small change to turnstile validation

--- backend/coordinators/User_gate.py
+++ backend/coordinators/User_gate.py
@@ -2,5 +2,6 @@ from backend.services import auth_services
 
 
 class User_gate:
-    def validate_turnstile_token(self, token: str) -> dict:
-        return auth_services.validate_turnstile_token(token)
+    def validate_turnstile_token(self, token: str) -> bool:
+        response = auth_services.validate_turnstile_token(token)
+        return response["success"]
--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash
+from backend.coordinators.User_gate import User_gate
 
 
 auth = Blueprint("auth", __name__)
@@ -9,14 +10,23 @@ def signup():
     if request.method == "GET":
         return render_template("signup.html")
     else:
-        username = request.form.get("username")
-        password = request.form.get("password")
-        turnstile_token = request.form.get("cf-turnstile-response")
-        if len(username) < 1: # type:ignore
+        form = request.form
+        username = form.get("username")
+        password = form.get("password")
+        turnstile_token = form.get("cf-turnstile-response")
+        user_gate = User_gate()
+        token_validity = user_gate.validate_turnstile_token(turnstile_token) # type:ignore
+        if not token_validity:
+            flash("We couldn't verify you are human, maybe try again?")
+            return redirect(url_for("auth.signup"))
+        elif len(username) < 1: # type:ignore
             flash("Enter a username")
             return redirect(url_for("auth.signup"))
         elif len(password) < 4: # type:ignore
             flash("password must be longer than 4 characters")
             return redirect(url_for("auth.signup"))
+        else:
+            # sign them up
+            return redirect(url_for("views.home_page"))
         # also check whether username already exists and redirect
         return "" # placeholder for now
