bdf081237d76969bf87e3224e4d1b5ddb0125c11
sink <sink_dev@proton.me>
2026-02-01 01:22:08 +0000

    add connection pool to flask app

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -3,6 +3,14 @@ from backend.routes.views import views
 from backend.endpoints.heartbeat_listener import heart_beat_listener
 from backend.endpoints.stock_data import stock_data
 from backend.routes.auth import auth
+from psycopg_pool import ConnectionPool
+import os
+from dotenv import find_dotenv, load_dotenv
+
+
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 
 
 def create_app():
@@ -15,5 +23,7 @@ def create_app():
     app.register_blueprint(heart_beat_listener)
     app.register_blueprint(stock_data, url_prefix="/stock_data/")
     app.register_blueprint(auth, url_prefix="/auth/")
+    
+    app.pg_connection_pool = ConnectionPool(f"host=localhost dbname=postgres user=postgres port=5432 password={POSTGRES_PASSWORD}", min_size=4, max_size=10) # type:ignore
     
     return app
