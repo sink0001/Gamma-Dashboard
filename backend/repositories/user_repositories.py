ceb1715f64d41be82ece91003af76b2c82807cf5
sink <sink_dev@proton.me>
2026-02-02 21:01:12 +0000

    make username_exists() function to check if a username exists in the database

--- backend/repositories/user_repositories.py
+++ backend/repositories/user_repositories.py
@@ -6,3 +6,10 @@ def create_user(username: str, password: str) -> None:
         conn.execute("INSERT INTO users (username, password) VALUES (%s, %s)",
                      (username, password)
                      )
+
+def username_exists(username: str) -> bool:
+    with current_app.pg_connection_pool.connection() as conn: # type:ignore
+        cur = conn.execute("SELECT * FROM users WHERE username = %s", [username])
+        if cur.fetchone():
+            return True
+        return False
