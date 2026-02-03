812b1b4cab0495081484954ceaa53ffac9ff6c6d
sink <sink_dev@proton.me>
2026-02-03 16:20:10 +0000

    make get_username_password() function to get the hashed password from a username

--- backend/repositories/user_repositories.py
+++ backend/repositories/user_repositories.py
@@ -7,9 +7,19 @@ def create_user(username: str, password: str) -> None:
                      (username, password)
                      )
 
+
 def username_exists(username: str) -> bool:
     with current_app.pg_connection_pool.connection() as conn: # type:ignore
         cur = conn.execute("SELECT * FROM users WHERE username = %s", [username])
         if cur.fetchone():
             return True
         return False
+ 
+
+def get_username_password(username: str) -> str:
+    with current_app.pg_connection_pool.connection() as conn: # type:ignore
+        cur = conn.execute("SELECT password FROM users WHERE username = %s", [username])
+        result = cur.fetchone()
+        if result:
+            return result[0]
+        return ""
