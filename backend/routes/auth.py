949b25ff8cc14dd75b3ca3faab3c7dd3f1ecc1ca
sink <sink_dev@proton.me>
2026-02-03 15:59:58 +0000

    make helper function to check_credential_format on entered username and password so length and format etc

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -5,6 +5,27 @@ from backend.coordinators.User_gate import User_gate
 auth = Blueprint("auth", __name__)
 
 
+def check_credential_format(username: str, password: str, turnstile_token_validity: bool) -> bool:
+    username_length = len(username) # type:ignore
+    password_length = len(password) # type:ignore
+    if not turnstile_token_validity:
+        flash("We couldn't verify you are human, maybe try again?")
+        return False
+    elif username_length < 1:
+        flash("Enter a username")
+        return False
+    elif username_length > 255:
+        flash("please enter a shorter username")
+        return False
+    elif password_length <= 4:
+        flash("password must be longer than 4 characters")
+        return False
+    elif password_length > 150:
+        flash("password shorter than 150 characters")
+        return False
+    return True
+
+
 @auth.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "GET":
@@ -17,31 +38,14 @@ def signup():
 
         user_gate = User_gate()
         token_validity = user_gate.validate_turnstile_token(turnstile_token) # type:ignore all these form components are guaranteed to be strings since the inputs are type text
-        
-        username_length = len(username) # type:ignore
-        password_length = len(password) # type:ignore
-
-        if not token_validity:
-            flash("We couldn't verify you are human, maybe try again?")
-            return redirect(url_for("auth.signup"))
-        elif username_length < 1:
-            flash("Enter a username")
-            return redirect(url_for("auth.signup"))
-        elif username_length > 255:
-            flash("please enter a shorter username")
-            return redirect(url_for("auth.signup"))
-        elif password_length < 4:
-            flash("password must be longer than 4 characters")
-            return redirect(url_for("auth.signup"))
-        elif password_length > 150:
-            flash("password shorter than 150 characters")
-            return redirect(url_for("auth.signup"))
-        else:
+        if check_credential_format(username, password, token_validity): # type:ignore
             success = user_gate.signup_user(username, password) # type:ignore
             if not success:
                 flash("a user with that username already exists")
                 return redirect(url_for("auth.signup"))
             return redirect(url_for("views.home_page"))
+        else:
+            return redirect(url_for("auth.signup"))
 
 
 @auth.route("/login", methods=["GET", "POST"])
