f0c3965a7f0c9fe0a692ad770b4656336d410952
sink <sink_dev@proton.me>
2026-02-09 16:53:47 +0000

    make remove_from_watchlist endpoint

--- backend/endpoints/user_info.py
+++ backend/endpoints/user_info.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, jsonify, session
+from flask import Blueprint, jsonify, session, request
 from flask_login import login_required, current_user
 
 
 user_info = Blueprint("user_info", __name__, url_prefix="/user_info/")
 
 
-@user_info.route("/add_ticker_to_watchlist", methods=["GET"])
+@user_info.route("/add_ticker_to_watchlist")
 @login_required
 def add_to_watchlist():
     try:
@@ -13,3 +13,12 @@ def add_to_watchlist():
         return jsonify(error=None, error_type=None, success=True), 200
     except Exception as e:
         return jsonify(error=e.args[0], error_type=type(e).__name__, success=False), 400
+    
+
+@user_info.route("/remove_ticker_from_watchlist", methods=["POST"])
+@login_required
+def remove_from_watchlist():
+    data = request.get_json()
+    ticker = data["ticker"]
+    current_user.remove_from_watchlist(ticker)
+    return ""
