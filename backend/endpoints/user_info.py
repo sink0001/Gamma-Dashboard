1ee6f92a383671e6b6acb41c5fef3f9e6774bf9a
sink <sink_dev@proton.me>
2026-02-09 13:09:32 +0000

    change add_ticker_to_watchlist to add the current ticker to the watchlist that the user last searched up

--- backend/endpoints/user_info.py
+++ backend/endpoints/user_info.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, session # 
 from flask_login import login_required, current_user
 from backend.models.User import User
 
@@ -6,13 +6,11 @@ from backend.models.User import User
 user_info = Blueprint("user_info", __name__, url_prefix="/user_info/")
 
 
-@user_info.route("/add_ticker_to_watchlist", methods=["POST"]) # only a ticker is given in the POST body
+@user_info.route("/add_ticker_to_watchlist", methods=["GET"])
 @login_required
 def add_to_watchlist():
     try:
-        data = request.json
-        ticker = data["ticker"] # type:ignore
-        current_user.add_stock_to_watchlist(ticker) # this raises a ValueError if the ticker is already in the 
-        return jsonify(error=None, success=True), 200
+        current_user.add_stock_to_watchlist(session["current_stock_ticker"]) # this raises a ValueError if the ticker is already in the 
+        return jsonify(error=None, error_type=None, success=True), 200
     except Exception as e:
         return jsonify(error=e.args[0], error_type=type(e).__name__, success=False), 400
