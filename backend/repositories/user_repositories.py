848ff944bee141c45a13ef0e5bf9f5e6197486aa
sink <sink_dev@proton.me>
2026-02-09 12:23:33 +0000

    make ticker_already_in_user_watchlist function

--- backend/repositories/user_repositories.py
+++ backend/repositories/user_repositories.py
@@ -43,9 +43,18 @@ def get_id_by_username(username: str) -> int | None:
         return None
  
 
-def add_to_user_watchlist(id: int, to_add: str) -> None:
+def add_to_user_watchlist(user_id: int, to_add: str) -> None:
     with current_app.pg_connection_pool.connection() as conn: # type:ignore
         conn.execute("""UPDATE users
                      SET stock_watchlist = array_append(stock_watchlist, %s)
-                     WHERE id = %s""", [to_add, id]
+                     WHERE id = %s""", [to_add, user_id]
                      )
+        
+
+def ticker_already_in_user_watchlist(user_id: int, ticker: str) -> bool:
+    with current_app.pg_connection_pool.connection() as conn: # type:ignore
+        cur = conn.execute("SELECT %s = ANY(stock_watchlist) FROM users WHERE id = %s", [ticker, user_id])
+        result = cur.fetchone()
+        if result:
+            return True
+        return False
